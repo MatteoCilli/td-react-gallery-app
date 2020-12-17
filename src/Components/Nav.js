c728d9959cf96dfa80fd4f5da2c5df1d3993420e
Matteo Cilli <cilli.matteo@gmail.com>
2020-12-17 12:36:58 +0100

    Strike Package Alpha

---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+function Nav() {
+    return (
+        <nav className="main-nav">
+            <ul>
+            <li><NavLink to="/planes">Planes</NavLink></li>
+            <li><NavLink to="/trains">Trains</NavLink></li>
+            <li><NavLink to="/automobiles">Automobiles</NavLink></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav 
---
+++ src/Components/PhotoContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Photo from './Photo'
+
+const PhotoContainer = props => {
+    const results = props.data
+    let photos = results.map(photo => {
+    const urlSource = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg` 
+    
+    return (<Photo url={urlSource}
+            key={photo.id}
+    />)
+    });
+
+    return(
+        <div className="photo-container">
+            <h2>Results</h2>
+            <ul>
+                {photos} 
+            </ul>
+            </div>
+    );
+};
+
+export default PhotoContainer;
